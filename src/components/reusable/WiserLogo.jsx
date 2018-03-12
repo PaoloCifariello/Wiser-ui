cf08de074a2e6fffce534d968cd4e6a5ea795e14
Paolo Cifariello <paolocifa@gmail.com>
2018-03-12 14:20:19 +0100

    WiserLogo component containing logo.

---
+++ src/components/reusable/WiserLogo.jsx
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import {Header, Image} from 'semantic-ui-react'
+
+import wiserLogo from '../wiser-logo.svg'
+
+const title = "Wiser",
+    subtitle = "Wikipedia Expertise Ranking";
+
+class WiserLogo extends Component {
+
+    render = () => {
+        return (
+            <div
+                style={{
+                fontFamily: "Roboto",
+                display: "flex",
+                textAlign: "center",
+                marginBottom: 20
+            }}>
+                <div>
+                    <Image src={wiserLogo} size='tiny'/>
+                </div>
+                <div>
+                    <Header as="h1"
+                        style={{
+                        marginBottom: -5,
+                        fontWeight: 300,
+                        fontVariant: "small-caps",
+                        fontSize: "5rem",
+                        marginBottom: -5,
+                        marginTop: -22
+                    }}>{title}</Header>
+                    <Header as="h3" style={{
+                        fontWeight: 300,
+                        marginTop: -10
+                    }}>{subtitle}</Header>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default WiserLogo;
