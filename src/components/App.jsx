15157b65d31f94c4b39d07056a1d41b8a0e5b56a
Paolo Cifariello <paolocifa@gmail.com>
2018-03-15 16:42:05 +0100

    Removed old code.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,12 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
 
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
-import {IndexLinkContainer, LinkContainer} from 'react-router-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
-import {Home, About, Profiles, AuthorProfile} from '.'
+import {Home, About, AuthorProfile} from '.'
 
 import './App.css';
 import './navbar.css';
-import wiserLogo from './wiser-logo.svg'
 
 class App extends Component {
   render() {
@@ -16,25 +15,11 @@ class App extends Component {
       <Router>
         <div>
           <Navbar className="margin-bottom-50">
-            {/* <Navbar.Header>
-              <Navbar.Brand>
-                <IndexLinkContainer to="/" text="Wiser">
-                  <a className="App-logo-container">
-                    <img src={wiserLogo} className="App-logo" alt="logo"/>
-                  </a>
-                </IndexLinkContainer>
-              </Navbar.Brand>
-            </Navbar.Header> */}
             <Nav>
               <LinkContainer exact to="/">
                 <NavItem>Home</NavItem>
               </LinkContainer>
             </Nav>
-            {/* <Nav>
-              <LinkContainer exact to="/profiles">
-                <NavItem>Profiles</NavItem>
-              </LinkContainer>
-            </Nav> */}
             <Nav pullRight>
               <LinkContainer exact to="/about">
                 <NavItem>About</NavItem>
@@ -48,7 +33,7 @@ class App extends Component {
               <Route exact path="/profile/:authorId" component={AuthorProfile}/>
               <Route exact path="/search" component={Home}/>
               <Route exact path="/search/:searchBy(e|n)/:query(.+)/" component={Home}/>
-              <Route path="*" render={() => (<Redirect to="/search" />)} /> 
+              <Route path="*" render={() => (<Redirect to="/search"/>)}/>
             </Switch>
           </div>
         </div>
