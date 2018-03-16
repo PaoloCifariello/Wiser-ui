c7cf01b8fb759cf5a46337eeee46b8b67779a160
Paolo Cifariello <paolocifa@gmail.com>
2018-03-16 16:51:55 +0100

    Removed old Profiles component.

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,3 @@
 export {default as Search} from './search/Search'
-export {default as Profiles} from './profiles/Profiles'
 export {default as AuthorProfile} from './profile/AuthorProfile'
 export {default as About} from './about/About'
