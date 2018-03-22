536aa07cdee0b7598c0c9c40bafbb571172d8e71
Paolo Cifariello <paolocifa@gmail.com>
2018-03-22 10:06:21 +0100

    Computing REC-IAF for entities.

--- src/components/reusable/Entity.jsx
+++ src/components/reusable/Entity.jsx
@@ -6,4 +6,6 @@ const renderEntityLink = (entityName) => {
     return <a target="_blank" href={`https://en.wikipedia.org/wiki/${entityName}`}>{normalizeEntityName(entityName)}</a>
 }
 
-export {normalizeEntityName, renderEntityLink};
+const computeEnttyReciaf = (entity) => entity["document_count"] * Math.sqrt(entity["pr_score"]) * entity["iaf"]
+
+export {normalizeEntityName, renderEntityLink, computeEnttyReciaf};
