d6ff6ad84553d2c6f4445edf1d78e50b8643a2de
Paolo Cifariello <paolocifa@gmail.com>
2018-03-22 10:06:53 +0100

    Survey for author entities.

--- src/components/profile/AuthorSurvey.jsx
+++ src/components/profile/AuthorSurvey.jsx
@@ -1,13 +1,63 @@
 import React, {Component} from 'react';
 
+import {Card, Rating} from 'semantic-ui-react'
+
+import api from '../../api/api'
+import {normalizeEntityName, computeEnttyReciaf} from '../reusable/Entity'
+
+const TOP_K_TOPICS = 30;
+
 class AuthorSurvey extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            authorTopics: []
+        }
+    }
+    componentDidMount = () => {
+        const {authorId} = this.props.authorInformation;
+
+        api
+            .getAuthorTopics(authorId)
+            .then((res) => {
+                const authorTopics = res
+                    .data
+                    .topics
+                    .map((topic) => {
+                        topic["importance_score"] = Math.log(1 + computeEnttyReciaf(topic))
+                        return topic
+                    })
+                    .sort((topic1, topic2) => topic2["importance_score"] - topic1["importance_score"])
+                this.setState({authorTopics: authorTopics})
+            });
+    }
+
+    renderAuthorTopic = ({entity_id, entity_name, document_count}) => {
+        return <Card
+            style={{
+            width: "calc(25% - 14px)",
+            minWidth: 150
+        }}
+            key={entity_id}>
+            <Card.Content>
+                <Card.Header>
+                    <a target="_blank" href={`https://en.wikipedia.org/wiki/${entity_name}`}>{normalizeEntityName(entity_name)}</a>
+                </Card.Header>
+                <Card.Description><Rating maxRating={5} icon='star' size='massive'/></Card.Description>
+            </Card.Content>
+            <Card.Content extra>{`Appears in ${document_count} documents`}</Card.Content>
+        </Card>
     }
 
     render = () => {
+        const {authorTopics} = this.state;
+
         return (
-            <div>Awesome survey</div>
+            <Card.Group>
+                {authorTopics
+                    .slice(0, TOP_K_TOPICS)
+                    .map(this.renderAuthorTopic)}
+            </Card.Group>
         )
     }
 }
