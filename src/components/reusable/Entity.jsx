410ad59f948aa3a0b8e96a952b0e9eb2640b0d91
Paolo Cifariello <paolocifa@gmail.com>
2018-03-29 18:02:30 +0200

    typo.

--- src/components/reusable/Entity.jsx
+++ src/components/reusable/Entity.jsx
@@ -6,6 +6,6 @@ const renderEntityLink = (entityName) => {
     return <a target="_blank" href={`https://en.wikipedia.org/wiki/${entityName}`}>{normalizeEntityName(entityName)}</a>
 }
 
-const computeEnttyReciaf = (entity) => entity["document_count"] * Math.sqrt(entity["pr_score"]) * entity["iaf"]
+const computeEntityReciaf = (entity) => entity["document_count"] * Math.sqrt(entity["pr_score"]) * entity["iaf"]
 
-export {normalizeEntityName, renderEntityLink, computeEnttyReciaf};
+export {normalizeEntityName, renderEntityLink, computeEntityReciaf};
