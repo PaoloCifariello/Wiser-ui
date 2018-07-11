1b7bae3eca59187340b9669d215be209c1a3c2b5
Paolo Cifariello <paolocifa@gmail.com>
2018-07-11 11:27:36 +0200

    eng translations for author roles.

--- src/components/reusable/AuthorRole.js
+++ src/components/reusable/AuthorRole.js
@@ -1,10 +1,10 @@
 const AUTHOR_ROLE = {
-    "0": "Altro personale docente",
-    "1": "Ricercatore",
-    "2": "Ricercatore a tempo determinato",
-    "3": "Docente di ruolo di Ia fascia",
-    "4": "Docente di ruolo di IIa fascia",
-    "5": "Professore a tempo determinato"
+    "0": "Others", //Altro personale docente
+    "1": "Researcher", //"Ricercatore",
+    "2": "Researcher", //"Ricercatore a tempo determinato",
+    "3": "Professor", //"Docente di ruolo di Ia fascia",
+    "4": "Professor", //"Docente di ruolo di IIa fascia",
+    "5": "Professor" //"Professore a tempo determinato"
 };
 
 export default AUTHOR_ROLE;
