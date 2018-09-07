cb90e9b3cb3c25f4768012e2fde32f314b2f5417
Paolo Cifariello <paolocifa@gmail.com>
2018-09-07 09:53:07 +0100

    [Bugfix] Importance help typo.

--- src/components/profile/AuthorTopics.jsx
+++ src/components/profile/AuthorTopics.jsx
@@ -19,7 +19,7 @@ import {normalizeEntityName} from '../reusable/Entity'
 import {inRange} from '../reusable/Util'
 
 
-const importanceHelp = (<span><b>Wiser score</b> is a measure of importance that depends on the amount of publications and the commonness of a certaing topic.</span>);
+const importanceHelp = (<span><b>Wiser score</b> is a measure of importance that depends on the amount of publications and the commonness of a certain topic.</span>);
 
 class AuthorTopics extends Component {
     constructor(props) {
